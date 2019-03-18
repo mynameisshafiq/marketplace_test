d5be3f3f03739646cb093e2e4ffe517440b89269
victordelval <victor.val.andres@gmail.com>
2019-03-18 18:11:53 +0100

    Improving Home page with new market form dialog

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Auth, Hub } from 'aws-amplify';
 import { Authenticator, AmplifyTheme } from 'aws-amplify-react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
+import MarketPage from './pages/MarketPage';
+import Navbar from './components/Navbar';
 import "./App.css";
 
 class App extends React.Component {
@@ -37,11 +42,32 @@ class App extends React.Component {
     }
   }
 
+  handleSignout = async () => {
+    try {
+      await Auth.signOut();
+    } catch(err) {
+      console.error('Error signing out user', err);
+    }
+  }
+
   render() {
     const { user } = this.state;
-    return !user ? (
-      <Authenticator theme={theme} /> 
-    ) : <div>App</div>;
+    return !user ? <Authenticator theme={theme} /> : (
+      <Router>
+        <>
+          {/* Navigation */}
+          <Navbar user={user} handleSignout={this.handleSignout} />
+          {/* Routes */}
+          <div className="app-container">
+            <Route exact path="/" component={HomePage} />
+            <Route path="/profile" component={ProfilePage} />
+            <Route path="/markets/:marketId" component={
+              ({ match }) => <MarketPage marketId={match.params.marketId} />
+            } />
+          </div>
+        </>
+      </Router>
+    );
   }
 }
 
